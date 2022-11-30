3b7fc5b3245b3fc5a5a27e5d091158841df32cb4
marketSnbv <110613994+marketSnbv@users.noreply.github.com>
2022-12-01 00:44:22 +0300

    Update count_messages.py

--- count_messages.py
+++ count_messages.py
@@ -2,17 +2,17 @@ import operator
 
 
 results = {}
-prevLine = ""
+prev_line = ""
 with open('result.json', encoding='utf-8') as f:
     for line in f:
         line = line.strip().replace('\"', '').replace(',', '').replace(' ', ''). split(':')
         if line[0] == 'from_id':
             if line[1] not in results:
-                results[line[1]] = [1, prevLine]
+                results[line[1]] = [1, prev_line]
             else:
                 results[line[1]][0] += 1
         elif line[0] == 'from':
-            prevLine = line[1]
+            prev_line = line[1]
 sorted_results = reversed(sorted(results.items(), key=operator.itemgetter(1)))
 for x in sorted_results:
     print(x[1])
